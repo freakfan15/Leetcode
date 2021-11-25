1bb20114621842e838d620a86516f8c701042232
freakfan15 <vivekkrrai.bxr@gmail.com>
2021-11-25 19:11:45 +0530

    testing

---
+++ 14 days of DS/max_subarray.cpp
@@ -0,0 +1,18 @@
+// 53. Maximum subarray
+#include<iostream>
+using namespace std;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int n;
+	cin>>n;
+	cout<<n;
+
+return 0;
+
+}
